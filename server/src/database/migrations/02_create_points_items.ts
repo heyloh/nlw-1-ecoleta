7465f0c2f9256cdf4585c20d937fc6f6ff099438
Lohana Torres <lohana100739@gmail.com>
2020-06-07 13:27:53 -0300

    Aplicação concluída

---
+++ server/src/database/migrations/02_create_points_items.ts
@@ -0,0 +1,23 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+  //criar tabela
+  return knex.schema.createTable('points_items', table => {
+    table.increments('id').primary();
+
+    table.integer('point_id')
+      .notNullable()
+      .references('id')
+      .inTable('points');
+
+    table.integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items');
+  });
+}
+
+export async function down(knex: Knex){
+  //voltar atrás (deletar tabela)
+  return knex.schema.dropTable('points_items');
+}
